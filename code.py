4c40c3fbf98afb4b855183962f8ed84d9adc8f85
ae25280-cmyk <ae25280@mbc.school.nz>
2026-07-01 09:39:56 +1200

    Add files via upload

---
+++ code.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from tkinter import messagebox
+
+MAIN_BG = "#ffffff"
+MAIN_FG = "#333333"
+BUTTON_BG = "#454853"
+BUTTON_FG = "#b0a5a5"
+TEXT_BG = "#f0f0f0"
+TEXT_FG = "#333333"
+TEXT_FONT = ("Arial", 12)
